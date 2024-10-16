fc1bcb02165824a5d81013319cfbdb4946e4908f
SatuNaja <136414469+Pluem10@users.noreply.github.com>
2024-10-16 17:06:35 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer" id="footer">
+      <div className="top-footer">
+        <h1>Nitiphon.</h1>
+      </div>
+      <div className="middle-footwe">
+        <ul className="foot-menu">
+
+        </ul>
+
+      </div>
+      <div className="footer-social-icons">
+          
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
