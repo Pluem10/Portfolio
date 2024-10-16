28a62317de80686b605ea9c8d258336e60ef79e6
SatuNaja <136414469+Pluem10@users.noreply.github.com>
2024-10-16 17:07:25 +0700

    Update ProjectsBox.jsx

--- src/components/ProjectsBox.jsx
+++ src/components/ProjectsBox.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const ProjectsBox = () => {
-  return <div>ProjectsBox</div>;
+  return (
+    <section className="section" id="project">
+      <div className="top-header">
+        <h1>Project</h1>
+      </div>
+      <div className="project-container">
+        <div className="project-box">
+          <i className="uil uil-briefcase-alt"></i>
+          <h3>Clients</h3>
+          <label>25+happy</label>
+        </div>
+        <div className="project-box">
+          <i className="uil uil-briefcase-alt"></i>
+          <h3>Clients</h3>
+          <label>25+happy</label>
+        </div>
+        <div className="project-box">
+          <i className="uil uil-briefcase-alt"></i>
+          <h3>Clients</h3>
+          <label>25+happy</label>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProjectsBox;
