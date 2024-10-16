9ecc4cd83c97bd1bb01e1bbc0a00353eba3d5fdd
SatuNaja <136414469+Pluem10@users.noreply.github.com>
2024-10-16 17:07:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,58 @@ import ContactBox from "./components/ContactBox";
 import Footer from "./components/Footer";
 import React, { useEffect } from "react";
 import Typed from "typed.js";
+import ScrollReveal from "scrollreveal";
 
 function App() {
   useEffect(() => {
+    /** Change active link */
+    const sections = document.querySelectorAll("section[id]");
+    const scrollActive = () => {
+      const scrollY = window.scrollY;
+      sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]")
+            .classList.add("active-link");
+        } else {
+          document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]")
+            .classList.remove("active-link");
+        }
+      });
+    };
+
+    /** scroll reveal */
+    const sr = ScrollReveal({
+      origin: "top",
+      distance: "80px",
+      duration: 2000,
+      reset: true,
+    });
+
+    // HOME
+    sr.reveal(".feature-text-card", {});
+    sr.reveal(".feature-name", {});
+
+    window.addEventListener("scroll", scrollActive);
+
     // type Effect
     const typingEffect = new Typed(".typedText", {
-      strings: ["lecturer", "Developer", "Researcher"],
+      strings: ["Lecturer", "Developer", "Researcher"],
       loop: true,
-      typSpeed: 100,
-      backspeed: 80,
+      typeSpeed: 100,
+      backSpeed: 80,
       backDelay: 2000,
     });
+
     return () => {
       typingEffect.destroy();
     };
-  });
+  }, []);
+
   return (
     <div>
       <div className="App">
